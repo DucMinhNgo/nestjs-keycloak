fb375fd82998e33deb999b159d98ed68857cae6b
ducngo <duc.ngo@salto.vn>
2024-08-14 10:45:14 +0700

    feat: setting client id for admin and user keycloak

--- SETUP.md
+++ SETUP.md
@@ -3,3 +3,20 @@
 # DOCKER:
 
 - https://github.com/cryoutsolutions/angular-nestjs/blob/master/docker-compose.yml
+
+# KeyCloak
+
+- Identity Federation and Single Sign-On (SSO)
+- https://medium.com/devops-dudes/secure-nestjs-rest-api-with-keycloak-745ef32a2370
+
+![alt text](images/image.png)
+
+![alt text](images/image2.png)
+
+# Postman
+
+- Access and get Users https://www.youtube.com/watch?v=lfBYdN79M9c
+
+![alt text](images/image1.png)
+
+- Create two client for user and for admin (and use postman to access them)
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,55 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard, KeycloakConnectModule, ResourceGuard, RoleGuard } from 'nest-keycloak-connect';
+import { APP_GUARD } from '@nestjs/core';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    KeycloakConnectModule.register({
+      authServerUrl: 'keycloak_web:8080/auth',
+      realm: 'nest-master',
+      clientId: 'nest-client',
+      secret: 'secret',
+      // optional if you want to retrieve JWT from cookie
+      cookieKey: 'KEYCLOAK_JWT',
+      // available on Realm Settings -> Keys -> RS256. then click "Public Key" button ;-)
+      realmPublicKey:
+        'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEArTgJ9uu6fCXAeNlPqb28BCAB05BcWZy1QUwB2sMsK6tRThPQ8gOUE62m/MVsIOlfEClETmKKipcSAuLnqDHIE8RZ9uYhA+sv2VQnzZCjxcS5aOWVkWJhjospMNK7vSMkfrPP9DpkLK+B+/ArXL9zb5gjs8/oQSLATu5enS1s1TLl91mrdNo1u+P0twplnDSbTkjI6qUdNpBLiH0s7dD9cHB9p0lU4gHb+VnrJ6qNqEuBgXJwrP8iAGYYqeiCAbcpWRIcPoBZE5HXrAM+CK5kVcroccgE8X3e9Z90taSW6MbhhpFXeva+ZO6j7ikCqFjJnziqdcw6+UpiG8rNmA8FAwIDAQAB',
+    }),
+    UsersModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    // These are in order, see https://docs.nestjs.com/guards#binding-guards
+    // for more information
+
+    // This adds a global level authentication guard, you can also have it scoped
+    // if you like.
+    //
+    // Will return a 401 unauthorized when it is unable to
+    // verify the JWT token or Bearer header is missing.
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard,
+    },
+    // This adds a global level resource guard, which is permissive.
+    // Only controllers annotated with @Resource and methods with @Scopes
+    // are handled by this guard.
+    {
+      provide: APP_GUARD,
+      useClass: ResourceGuard,
+    },
+    // New in 1.1.0
+    // This adds a global level role guard, which is permissive.
+    // Used by `@Roles` decorator with the optional `@AllowAnyRole` decorator for allowing any
+    // specified role passed.
+    {
+      provide: APP_GUARD,
+      useClass: RoleGuard,
+    },
+    AppService
+  ],
 })
-export class AppModule {}
+export class AppModule { }
